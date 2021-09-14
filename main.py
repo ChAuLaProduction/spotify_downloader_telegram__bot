75b552f093ec136c6c5b67c19e5370a832e5d667
ChAuLaProduction <86785480+ChAuLaProduction@users.noreply.github.com>
2021-09-15 01:53:56 +0530

    Update main.py

--- main.py
+++ main.py
@@ -63,7 +63,7 @@ def START(msg, chat_id):
         downloader(msg, chat_id, 'AR')
 
     elif msg == "/start":
-        bot.sendMessage(chat_id,"Hi `{ok.user.first_name}`\nsend me spotify link and I'll give you music\nor use /single or /album or /artist")
+        bot.sendMessage(chat_id,"Hi \nsend me spotify link and I'll give you music\nor use /single or /album or /artist")
 
 
     elif msg == "/album":
